602c6e57dc3f1e9bc8033e45c7382c5cfcdc3260
ndmkcn <ndmkcn1453@gmail.com>
2023-04-07 00:17:57 +0300

    Payment page amount and amount information is done

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
+import { CartService } from 'src/app/services/cart.service';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 
@@ -24,9 +25,11 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
-  constructor(private formBuilder:FormBuilder,private luv2ShopFormService:Luv2ShopFormService) { }
+  constructor(private formBuilder:FormBuilder,private luv2ShopFormService:Luv2ShopFormService,private cartService:CartService) { }
 
   ngOnInit(): void {
+
+    this.reviewCartDetails();
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
@@ -81,6 +84,14 @@ export class CheckoutComponent implements OnInit {
       }
     );
 
+  }
+  reviewCartDetails() {
+    this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
+    );
+    this.cartService.totalQuantity.subscribe(
+      totalQuantity => this.totalQuantity = totalQuantity
+    );
   }
 
   copyShippingAddressToBillingAddress(event: any) {
